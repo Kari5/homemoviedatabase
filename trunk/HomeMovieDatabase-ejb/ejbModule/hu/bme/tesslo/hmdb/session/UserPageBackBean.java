7199aa078e0af17046251d93981431be49f30919
winkari5@gmail.com <winkari5@gmail.com@57e50dc0-6129-96d0-ffb8-4e4f1a8ae0d2>
2012-11-03 15:15:49 +0000

    értékelés törlése, plussz a film lista nem kell conversationben legyen, elég neki a page scope

--- trunk/HomeMovieDatabase-ejb/ejbModule/hu/bme/tesslo/hmdb/session/UserPageBackBean.java
+++ trunk/HomeMovieDatabase-ejb/ejbModule/hu/bme/tesslo/hmdb/session/UserPageBackBean.java
@@ -1,16 +1,17 @@
 package hu.bme.tesslo.hmdb.session;
 
+import hu.bme.tesslo.hmdb.dao.RatingDao;
+import hu.bme.tesslo.hmdb.dao.UserDao;
+import hu.bme.tesslo.hmdb.model.Movie;
+import hu.bme.tesslo.hmdb.model.Rating;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-import hu.bme.tesslo.hmdb.dao.RatingDao;
-import hu.bme.tesslo.hmdb.dao.UserDao;
-import hu.bme.tesslo.hmdb.model.Movie;
-import hu.bme.tesslo.hmdb.model.Rating;
-
+import org.jboss.logging.Logger;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Create;
 import org.jboss.seam.annotations.In;
@@ -24,31 +25,46 @@ public class UserPageBackBean {
 
 	@In
 	Credentials credentials;
-	
+
 	private UserDao userDao;
-	
+
 	private RatingDao ratingDao;
-	
+
 	private List<Movie> favoriteMovies;
-	
+
 	private List<Rating> ownRatings;
-	
+
+	private static final Logger logger = Logger
+			.getLogger(UserPageBackBean.class);
+
 	@Create
-	public void init(){
-		this.favoriteMovies=new ArrayList<Movie>();
-		this.ownRatings=new ArrayList<Rating>();
-		
+	public void init() {
+		this.favoriteMovies = new ArrayList<Movie>();
+		this.ownRatings = new ArrayList<Rating>();
+
 		try {
-			userDao=(UserDao)InitialContext.doLookup("HomeMovieDatabase-ear/UserDao/local");
-			favoriteMovies=userDao.favoriteMovies(this.credentials.getUsername());
-			
+			userDao = (UserDao) InitialContext
+					.doLookup("HomeMovieDatabase-ear/UserDao/local");
+			favoriteMovies = userDao.favoriteMovies(this.credentials
+					.getUsername());
+
 			ratingDao = InitialContext
 					.doLookup("HomeMovieDatabase-ear/ratingDao/local");
-			ownRatings=ratingDao.userRatings(this.credentials.getUsername());
+			ownRatings = ratingDao.userRatings(this.credentials.getUsername());
 		} catch (NamingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(e.getMessage(), e);
 		}
+	}
+
+	public void deleteRating(Rating rating) {
+		ratingDao.deleteRating(rating);
+		// try {
+		// ratingDao = InitialContext
+		// .doLookup("HomeMovieDatabase-ear/ratingDao/local");
+		// } catch (NamingException e) {
+		// logger.error(e.getMessage(), e);
+		// }
+		// ownRatings = ratingDao.userRatings(this.credentials.getUsername());
 	}
 
 	/**
@@ -59,7 +75,8 @@ public class UserPageBackBean {
 	}
 
 	/**
-	 * @param favoriteMovies the favoriteMovies to set
+	 * @param favoriteMovies
+	 *            the favoriteMovies to set
 	 */
 	public void setFavoriteMovies(List<Movie> favoriteMovies) {
 		this.favoriteMovies = favoriteMovies;
@@ -73,11 +90,11 @@ public class UserPageBackBean {
 	}
 
 	/**
-	 * @param ownRatings the ownRatings to set
+	 * @param ownRatings
+	 *            the ownRatings to set
 	 */
 	public void setOwnRatings(List<Rating> ownRatings) {
 		this.ownRatings = ownRatings;
 	}
-	
-	
+
 }
