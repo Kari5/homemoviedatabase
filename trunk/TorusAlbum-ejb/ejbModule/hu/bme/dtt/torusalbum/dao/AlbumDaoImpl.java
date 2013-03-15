d451990149d217700879ccb2f67de2745125f163
winkari5@gmail.com <winkari5@gmail.com@57e50dc0-6129-96d0-ffb8-4e4f1a8ae0d2>
2013-03-15 11:46:53 +0000

    Alap entitások, és hozzá dao-k. Még nem végleges

--- trunk/TorusAlbum-ejb/ejbModule/hu/bme/dtt/torusalbum/dao/AlbumDaoImpl.java
+++ trunk/TorusAlbum-ejb/ejbModule/hu/bme/dtt/torusalbum/dao/AlbumDaoImpl.java
@@ -1,5 +1,25 @@
 package hu.bme.dtt.torusalbum.dao;
 
-public class AlbumDaoImpl implements AlbumDao {
+import hu.bme.dtt.torusalbum.entity.Album;
+import hu.futurion.mt.dao.GenericDaoImpl;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.jboss.logging.Logger;
+
+@Stateless(name = "AlbumDao")
+public class AlbumDaoImpl extends GenericDaoImpl<Album> implements AlbumDao {
+
+	@PersistenceContext(unitName = "TorusAlbum")
+	EntityManager entityManager;
+
+	private static final Logger logger = Logger.getLogger(AlbumDaoImpl.class);
+
+	@Override
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
 
 }
--- trunk/TorusAlbum-ejb/ejbModule/hu/bme/dtt/torusalbum/dao/PictureDaoImpl.java
+++ trunk/TorusAlbum-ejb/ejbModule/hu/bme/dtt/torusalbum/dao/PictureDaoImpl.java
@@ -3,10 +3,28 @@
  */
 package hu.bme.dtt.torusalbum.dao;
 
+import hu.bme.dtt.torusalbum.entity.Picture;
+import hu.futurion.mt.dao.GenericDaoImpl;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.jboss.logging.Logger;
+
 /**
  * @author Karcsi
- *
+ * 
  */
-public class PictureDaoImpl implements PcitureDao {
+public class PictureDaoImpl extends GenericDaoImpl<Picture> implements
+		PictureDao {
 
+	@PersistenceContext(unitName = "TorusAlbum")
+	EntityManager entityManager;
+
+	private static final Logger logger = Logger.getLogger(PictureDaoImpl.class);
+
+	@Override
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
 }
