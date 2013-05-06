5e1e0ac664e6e66ec42756af104ddff5cf0f5f50
winkari5 <winkari5@gmail.com>
2013-05-06 20:45:06 +0000

    back bean

--- TorusAlbum-ejb/ejbModule/hu/bme/dtt/torussearch/session/SearchBackBean.java
+++ TorusAlbum-ejb/ejbModule/hu/bme/dtt/torussearch/session/SearchBackBean.java
@@ -3,11 +3,13 @@
  */
 package hu.bme.dtt.torussearch.session;
 
+import hu.bme.dtt.torussearch.searchco.SearchCo;
 import hu.bme.dtt.util.Result;
 
 import java.util.List;
 
 import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Create;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
@@ -21,12 +23,36 @@ public class SearchBackBean {
 
 	private List<Result> resultList;
 	
+	private String query;
+	
+	
+	@Create
+	public void init(){
+		SearchCo sc=new SearchCo();
+		resultList=sc.searchCode("java", null, null, null, null);
+	}
+	
+	public void search(){
+		SearchCo sc=new SearchCo();
+		if(query!=null)
+		resultList=sc.searchCode(query, null, null, null, null);
+	}
+	
+	
 	public List<Result> getResultList() {
 		return resultList;
 	}
 	
 	public void setResultList(List<Result> resultList) {
 		this.resultList = resultList;
+	}
+	
+	public String getQuery() {
+		return query;
+	}
+	
+	public void setQuery(String query) {
+		this.query = query;
 	}
 	
 }
