3b8161333e082dae507a3ece70a2a34c606f11f6
winkari5@gmail.com <winkari5@gmail.com@57e50dc0-6129-96d0-ffb8-4e4f1a8ae0d2>
2013-05-06 21:28:38 +0000

    back bean-be lehet váltogatni a google és a sc között

--- trunk/TorusAlbum-ejb/ejbModule/hu/bme/dtt/torussearch/session/SearchBackBean.java
+++ trunk/TorusAlbum-ejb/ejbModule/hu/bme/dtt/torussearch/session/SearchBackBean.java
@@ -3,6 +3,7 @@
  */
 package hu.bme.dtt.torussearch.session;
 
+import hu.bme.dtt.torussearch.googlesearch.GoogleSearch;
 import hu.bme.dtt.torussearch.searchco.SearchCo;
 import hu.bme.dtt.util.Result;
 
@@ -25,17 +26,24 @@ public class SearchBackBean {
 	
 	private String query;
 	
+	private boolean googleEanbled;
+	
 	
 	@Create
 	public void init(){
-		SearchCo sc=new SearchCo();
-		resultList=sc.searchCode("java", null, null, null, null);
+//		SearchCo sc=new SearchCo();
+//		resultList=sc.searchCode("java", null, null, null, null);
 	}
 	
 	public void search(){
 		SearchCo sc=new SearchCo();
+		GoogleSearch gs=new GoogleSearch();
 		if(query!=null)
+			if(googleEanbled){
+				resultList = gs.executeSearch(query, null, null, null);
+			} else {
 		resultList=sc.searchCode(query, null, null, null, null);
+			}
 	}
 	
 	
@@ -53,6 +61,14 @@ public class SearchBackBean {
 	
 	public void setQuery(String query) {
 		this.query = query;
+	}
+	
+	public boolean isGoogleEanbled() {
+		return googleEanbled;
+	}
+	
+	public void setGoogleEanbled(boolean googleEanbled) {
+		this.googleEanbled = googleEanbled;
 	}
 	
 }
