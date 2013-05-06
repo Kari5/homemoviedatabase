f3909866540e9c1ca38a38bd238510049ea90b19
shazak.tt@gmail.com <shazak.tt@gmail.com@57e50dc0-6129-96d0-ffb8-4e4f1a8ae0d2>
2013-05-06 21:13:44 +0000

    goo9gle alapú search

---
+++ trunk/TorusAlbum-ejb/ejbModule/hu/bme/dtt/torussearch/googlesearch/GoogleSearch.java
@@ -0,0 +1,74 @@
+package hu.bme.dtt.torussearch.googlesearch;
+
+import hu.bme.dtt.torusalbum.dao.SearchEngineDao;
+import hu.bme.dtt.torusalbum.entity.SearchEngine;
+import hu.bme.dtt.torusalbum.util.search.Search;
+import hu.bme.dtt.torusalbum.util.search.model.Response;
+import hu.bme.dtt.torusalbum.util.search.model.item.Item;
+import hu.bme.dtt.util.Result;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.jboss.logging.Logger;
+
+public class GoogleSearch {
+	private static final Logger LOGGER = Logger.getLogger(GoogleSearch.class);
+
+	/**
+	 * 
+	 * @param query
+	 *            The query string.
+	 * @param fileType
+	 *            The extension of the file type to search for.
+	 * @param url
+	 *            The url of the site to search on.
+	 * @param lang
+	 *            The code language to search for.
+	 * @return
+	 */
+	public List<Result> executeSearch(String query, String fileType, String url, String lang) {
+		List<Result> resultList = new ArrayList<Result>();
+		try {
+			SearchEngine searchEngine = ((SearchEngineDao) InitialContext
+					.doLookup("TorusAlbum-ear/searchEngineDao/local")).getSearchEngine("Torus");
+			Search search = new Search();
+			Map<String, Object> searchParams = new HashMap<String, Object>();
+			searchParams.put("q", URLEncoder.encode(query + " " + lang, "UTF-8"));
+			searchParams.put("key", searchEngine.getApiKey());
+			searchParams.put("cx", searchEngine.getCxKey());
+			searchParams.put("fileType", fileType);
+			searchParams.put("siteSearch", url);
+			searchParams.put("alt", "json");
+			try {
+				Response response = search.executeSearch(searchParams);
+				for (Item item : response.getItems()) {
+					Result result = new Result();
+					result.setName(item.getHtmlTitle());
+					result.setUrl(item.getHtmlFormattedUrl());
+					result.setSnippet(item.getHtmlSnippet());
+					resultList.add(result);
+				}
+			} catch (IllegalArgumentException e) {
+				LOGGER.error(e.getMessage(), e);
+			} catch (IllegalAccessException e) {
+				LOGGER.error(e.getMessage(), e);
+			} catch (IOException e) {
+				LOGGER.error(e.getMessage(), e);
+			}
+		} catch (NamingException e) {
+			LOGGER.error(e.getMessage(), e);
+		} catch (UnsupportedEncodingException e) {
+			LOGGER.error(e.getMessage(), e);
+		}
+		return resultList;
+	}
+}
