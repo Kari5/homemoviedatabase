af41ef56de68a1ef7c21c9cdd40d5d15df28f0f7
winkari5@gmail.com <winkari5@gmail.com@57e50dc0-6129-96d0-ffb8-4e4f1a8ae0d2>
2013-05-07 17:48:00 +0000

    javítások

--- trunk/TorusAlbum-ejb/ejbModule/hu/bme/dtt/torussearch/searchco/Search.java
+++ trunk/TorusAlbum-ejb/ejbModule/hu/bme/dtt/torussearch/searchco/Search.java
@@ -6,7 +6,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.jboss.logging.Logger;
+
 public class Search {
+	private static final Logger LOGGER = Logger.getLogger(Search.class);
 	private static final String SEARCH_CO_URL = "http://searchco.de/api";
 	private static final String SEARCH_CO_DOC = "/search_IV/?";
 	private static final String SEARCH_CO_CODE = "/codesearch_I/?";
@@ -45,12 +48,14 @@ public class Search {
 	 *             If connection fails.
 	 */
 	private String htmlGetRequest(String urlToRead) throws IOException {
+		LOGGER.info("URL to read: "+urlToRead);
 		URL url;
 		HttpURLConnection conn;
 		BufferedReader rd;
 		String line;
 		String result = "";
 		url = new URL(urlToRead);
+		LOGGER.info("Created URL: "+url);
 		conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 		rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
