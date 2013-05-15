2bc4757da77914b7848b319b84a717475888f49d
winkari5 <winkari5@gmail.com>
2013-05-15 09:46:32 +0000

    nevező algoritmus váza

---
+++ TorusAlbum-ejb/ejbModule/hu/bme/dtt/torusalbum/util/TitleFactory.java
@@ -0,0 +1,38 @@
+package hu.bme.dtt.torusalbum.util;
+
+import java.util.List;
+
+public class TitleFactory {
+
+	public static List<String> getMetaTags() {
+
+		return null;
+	}
+
+	public static List<String> commonMetaTags(List<List<String>> metaTagLists) {
+
+		return null;
+	}
+
+	public static List<String> getWebPageNames() {
+
+		return null;
+	}
+
+	public static List<String> commonWebPageNames(
+			List<List<String>> webPageNameLists) {
+
+		return null;
+	}
+	
+	public static List<String> getWebMetaTags(){
+		
+		return null;
+	}
+	
+	public static List<String> commonWebMetaTags(List<List<String>> webMetaTagLists){
+		
+		return null;
+	}
+
+}
