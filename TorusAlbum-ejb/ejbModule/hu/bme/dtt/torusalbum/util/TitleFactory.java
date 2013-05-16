116a06c18e82da4aac54f1d8ecd25b4e27dee1d7
winkari5 <winkari5@gmail.com>
2013-05-16 15:53:42 +0000

    tag + sorting + unio

--- TorusAlbum-ejb/ejbModule/hu/bme/dtt/torusalbum/util/TitleFactory.java
+++ TorusAlbum-ejb/ejbModule/hu/bme/dtt/torusalbum/util/TitleFactory.java
@@ -1,38 +1,43 @@
 package hu.bme.dtt.torusalbum.util;
 
+import java.util.Collections;
 import java.util.List;
 
 public class TitleFactory {
 
-	public static List<String> getMetaTags() {
+	public static List<Tag> getMetaTags() {
 
 		return null;
 	}
 
-	public static List<String> commonMetaTags(List<List<String>> metaTagLists) {
+	public static List<Tag> getWebPageNames() {
 
 		return null;
 	}
 
-	public static List<String> getWebPageNames() {
-
-		return null;
-	}
-
-	public static List<String> commonWebPageNames(
-			List<List<String>> webPageNameLists) {
-
-		return null;
-	}
 	
-	public static List<String> getWebMetaTags(){
+	public static List<Tag> getWebMetaTags(){
 		
 		return null;
 	}
 	
-	public static List<String> commonWebMetaTags(List<List<String>> webMetaTagLists){
-		
-		return null;
+	public static List<Tag> unioTagLists(List<List<Tag>> webMetaTagLists){
+		List<Tag> resultTags=webMetaTagLists.get(0);
+		for(int i=1; i< webMetaTagLists.size(); i++){
+			for(Tag t : webMetaTagLists.get(i)){
+				for(Tag resultTag : resultTags){
+					if(t.getName().equals(resultTag.getName())){
+						resultTag.raisCounter();
+					} else {
+						resultTags.add(t);
+					}
+				}
+			}
+		}
+		Collections.sort(resultTags);
+		return resultTags;
 	}
+	
+	
 
 }
